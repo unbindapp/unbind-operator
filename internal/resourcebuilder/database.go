487a7009013f0a4f11a63f2490eff9305a5e911a
Brandon Berhent <bbedward@gmail.com>
2025-05-10 13:11:36 -0400

    Fix secret ref

--- internal/resourcebuilder/database.go
+++ internal/resourcebuilder/database.go
@@ -47,15 +47,6 @@ func (rb *ResourceBuilder) BuildDatabaseObjects(ctx context.Context, logger logr
 		dbConfig["labels"] = make(map[string]string)
 	}
 
-	if rb.service.Spec.Config.Database.Type == "clickhouse" {
-		// Clickhouse can't have a cluster name > 15 chars so make chi-{serviceref} truncated to 15
-		clusterName := fmt.Sprintf("chi-%s", rb.service.Spec.ServiceRef)
-		if len(clusterName) > 15 {
-			clusterName = clusterName[:15]
-		}
-		dbConfig["clusterName"] = clusterName
-	}
-
 	labelsMap, ok := dbConfig["labels"].(map[string]interface{})
 	if !ok {
 		// Try to convert to the expected format
@@ -148,6 +139,17 @@ func (rb *ResourceBuilder) BuildDatabaseObjects(ctx context.Context, logger logr
 
 	if strings.EqualFold(rb.service.Spec.Config.Database.Type, "mongodb") {
 		dbConfig["existingSecretName"] = fmt.Sprintf("%s-mongo-secret", rb.service.Spec.ServiceRef)
+	}
+
+	if strings.EqualFold(rb.service.Spec.Config.Database.Type, "clickhouse") {
+		// Clickhouse can't have a cluster name > 15 chars so make chi-{serviceref} truncated to 15
+		clusterName := fmt.Sprintf("chi-%s", rb.service.Spec.ServiceRef)
+		if len(clusterName) > 15 {
+			clusterName = clusterName[:15]
+		}
+		dbConfig["clusterName"] = clusterName
+		// Same for secrets as mongo
+		dbConfig["existingSecretName"] = fmt.Sprintf("%s-clickhouse-secret", rb.service.Spec.ServiceRef)
 	}
 
 	// Database backups
